de26fab79300822abf41c945b4d1d5816a99a6c7
akazicin <akazicin@gmail.com>
2024-04-15 22:17:59 +0300

    Homework Object

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,11 @@ public class Main {
         Author author1 = new Author("Михаил", "Лермонтов");
         Book nameBook1 = new Book(author, "Парус", 1841);
 
+        Author author2 = new Author("Александр", "Пушкин");
+        Book nameBook2 = new Book(author2, "Капитанская дочка", 1836);
+
         System.out.println(nameBook);
         System.out.println(nameBook1);
+        System.out.println(nameBook2);
     }
 }
